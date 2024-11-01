f4048a95cad5d3e2d982909726622a8af5da332e
Loan Challeat <51877319+LoanClt@users.noreply.github.com>
2024-11-01 10:33:31 +0100

    Update main.m

--- main.m
+++ main.m
@@ -1,20 +1,81 @@
-% Afficher l'image de référence floue
+%% Test
+
+L = detectSpotCenters(ImageTest)
+i_molecules
+j_molecules
+
+%% Load
+
+load("CoordinatesTest.mat")
+load("ImageFloue.mat")
+load("ImagesPALM.mat")
+load("ImageTest.mat")
+
+% Paramètres
+upsampleFactor = 20; % Facteur de suréchantillonnage pour la super-résolution
+sigma = 1; % Ecart-type pour la pondération gaussienne
+intensityThreshold = 0; % Seuil d'intensité pour ignorer les points faibles
+
+% Initialisation de la matrice d'accumulation haute résolution
+[numRows, numCols, numFrames] = size(ImagesPALM);
+highResRows = numRows * upsampleFactor;
+highResCols = numCols * upsampleFactor;
+highResImage = zeros(highResRows, highResCols);
+
+% Définir la PSF gaussienne
+gaussianSize = round(6 * sigma);
+[x, y] = meshgrid(-gaussianSize:gaussianSize, -gaussianSize:gaussianSize);
+gaussianPSF = exp(-(x.^2 + y.^2) / (2 * sigma^2));
+
+% Boucle sur chaque image de la séquence
+for k = 1:numFrames
+    img = ImagesPALM(:,:,k);
+    
+    % Détecter les centres des photons
+    centers = detectSpotCenters(img);
+    
+    % Ajouter les localisations de photons avec précision sub-pixellaire
+    for i = 1:size(centers, 1)
+        % Position haute résolution sans arrondi
+        xHighRes = centers(i,1) * upsampleFactor;
+        yHighRes = centers(i,2) * upsampleFactor;
+        
+        % Vérifier l'intensité pour filtrer les points faibles
+        if img(round(centers(i,2)), round(centers(i,1))) > intensityThreshold
+            % Calculer les indices de position du centre pour le PSF
+            xRange = round(xHighRes)-gaussianSize:round(xHighRes)+gaussianSize;
+            yRange = round(yHighRes)-gaussianSize:round(yHighRes)+gaussianSize;
+            
+            % S'assurer que les indices sont dans les limites
+            if all(xRange > 0 & xRange <= highResCols) && all(yRange > 0 & yRange <= highResRows)
+                % Ajouter la PSF gaussienne autour du centre détecté
+                highResImage(yRange, xRange) = highResImage(yRange, xRange) + gaussianPSF;
+            end
+        end
+    end
+end
+
+% Redimensionner l'image haute résolution à la taille d'origine
+finalImage = highResImage;
+
+% Binariser l'image finale pour isoler les lettres
+level = graythresh(finalImage);
+binaryImage = imbinarize(finalImage, level);
+%binaryImage = finalImage; 
+
+% Appliquer un filtre morphologique pour lisser et réduire le bruit
+binaryImage = imclose(binaryImage, strel('disk', 1)); % Fermeture morphologique pour remplir les trous
+binaryImage = imopen(binaryImage, strel('disk', 1)); % Ouverture pour réduire les petits bruits
+
+% Afficher l'image finale binarisée et lissée
 figure;
-imshow(ImageFloue);
-title('Image de référence floue');
+imshow(binaryImage);
+title('Image binarisée améliorée avec lissage morphologique');
 
-% Reconstruire l'image PALM
-fprintf('Début de la reconstruction PALM...\n');
-reconstructedImage = reconstructPALM(ImagesPALM);
-
-% Afficher l'image reconstruite
+% Afficher l'image finale binarisée et lissée
 figure;
-imshow(reconstructedImage);
-title('Image PALM reconstruite');
-
-% Sauvegarder l'image reconstruite
-imwrite(reconstructedImage, 'palm_reconstruction.tif');
-fprintf('Reconstruction terminée et sauvegardée dans palm_reconstruction.tif\n');
+imshow(finalImage);
+title('Image binarisée améliorée sans lissage morphologique');
 
 % Fonction de détection des centres des spots
 function centers = detectSpotCenters(img)
@@ -34,42 +95,5 @@ function centers = detectSpotCenters(img)
     centers = zeros(num, 2);
     for i = 1:num
         centers(i,:) = props(i).Centroid;
-    end
-end
-
-% Fonction de reconstruction PALM
-function reconstructedImage = reconstructPALM(ImagesPALM, pixelSize)
-    % Paramètres
-    if nargin < 2
-        pixelSize = 0.1; % Taille du pixel dans l'image reconstruite (en unités originales)
-    end
-    % Déterminer la taille de l'image reconstruite
-    [height, width, numFrames] = size(ImagesPALM);
-    scaleFactor = 1/pixelSize;
-    newHeight = ceil(height * scaleFactor);
-    newWidth = ceil(width * scaleFactor);
-    % Initialiser l'image reconstruite
-    reconstructedImage = zeros(newHeight, newWidth);
-    % Traiter chaque image de la séquence
-    fprintf('Traitement des images en cours...\n');
-    for k = 1:numFrames
-        if mod(k, 100) == 0
-            fprintf('Image %d/%d\n', k, numFrames);
-        end
-        % Extraire l'image courante
-        currentImage = ImagesPALM(:,:,k);
-        % Détecter les centres des spots
-        centers = detectSpotCenters(currentImage);
-        % Ajouter chaque localisation à l'image reconstruite
-        for i = 1:size(centers, 1)
-            % Convertir les coordonnées à l'échelle super-résolue
-            x = round(centers(i,1) * scaleFactor);
-            y = round(centers(i,2) * scaleFactor);
-            % Vérifier que les coordonnées sont dans les limites
-            if x > 0 && x <= newWidth && y > 0 && y <= newHeight
-                % Ajouter un point gaussien
-                reconstructedImage(y,x) = reconstructedImage(y,x) + 1;
-            end
-        end
     end
 end
